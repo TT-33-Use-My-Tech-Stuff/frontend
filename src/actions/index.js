a97336a493ed0435df2c535135fd227a77c21664
paulstgermain <paulg.stgermain@gmail.com>
2021-03-01 18:28:37 -0500

    adds action/case in reducer, handles login data - connects Login component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 export const SUBMIT_SIGNUP = "SUBMIT_SIGNUP";
+export const SUBMIT_LOGIN = "SUBMIT_LOGIN"
 
 export function submitSignup(signupData){
-    return { type: SUBMIT_SIGNUP, payload: signupData}
+    return { type: SUBMIT_SIGNUP, payload: signupData};
+}
+
+export function submitLogin(loginData){
+    return{ type: SUBMIT_LOGIN, payload: loginData};
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { SUBMIT_SIGNUP } from '../actions';
+import { SUBMIT_SIGNUP, SUBMIT_LOGIN } from '../actions';
 
 const initialState = {
     title: 'Hello from the reducer!',
@@ -17,10 +17,15 @@ function reducer(state = initialState, action) {
     console.log(`action type: ${action.type}, action payload: ${action.payload}`)
     switch(action.type){
         case SUBMIT_SIGNUP:
-            console.log('submit signup payload: ' + action.payload)
             return {
                 ...state,
                 signup: action.payload
+            }
+
+        case SUBMIT_LOGIN:
+            return {
+                ...state,
+                login: action.payload
             }
         default:
             return state;
