8e1b81094e3d43041a77735f0258ee91fccd0c4d
paulstgermain <paulg.stgermain@gmail.com>
2021-03-02 17:15:20 -0500

    adds redux-thunk

--- src/components/validation/signupSchema.js
+++ src/components/validation/signupSchema.js
@@ -13,9 +13,9 @@ const signSchema = yup.object().shape({
     .string()
     .required('Password is required')
     .min(8, 'Password must be at least 8 characters long')
-    .matches(
-        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      "Password must contain at least 8 characters, one uppercase, one number and one special case character"
-    ),
+    // .matches(
+    //     /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+    //   "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+    // ),
 })
 export default signSchema
