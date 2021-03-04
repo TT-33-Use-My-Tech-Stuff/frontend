d3d78b106f7bbd90a4042994369b4491a27e4b0f
paulstgermain <paulg.stgermain@gmail.com>
2021-03-04 14:36:01 -0500

    styles techList page

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import { fetchTech } from '../actions';
@@ -17,16 +18,19 @@ function TechList(props){
 
     //* ADD BUTTON TO RENDER ADDITEM FORM
     return(
-        <div>
+        <StyledTechList>
             <h1>Available Rentals </h1>
             {currentUser.role === "Owner" && <button>Add New Tech</button>}
             {techList && techList.map(i => {
-                return (<div key={i.tech_id}>
+                return (<div className='techCard' key={i.tech_id}>
+                <div className='techText'>
                     <p>{i.name}</p>
                     <p>{i.description}</p>
+                </div>
+                <div className='techImg'><p>Item Image Here</p></div>
                 </div>)
             })}
-        </div>
+        </StyledTechList>
     )
 }
 
@@ -38,3 +42,39 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { fetchTech })(TechList);
+
+const StyledTechList = styled.div`
+    text-align: center;
+
+    .techCard{
+        width: 90%;
+        height: 15rem;
+        border: 1px solid white;
+        display: flex;
+        justify-content: space-between;
+        margin: 2.5rem auto;
+
+        .techText{
+            width: 74%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .techImg{
+            width: 25%;
+            height: 100%;
+            border: 1px solid white;
+            background: #9F9F9F;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+
+    .techCard:nth-child(even){
+        flex-direction: row-reverse;
+    }
+`
